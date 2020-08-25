baac5c13dc41e2a192e0e476084544659e3d26ce
Ramendra bharadwaj <bharadwaj.ramendra1@gmail.com>
2020-08-25 19:01:14 +0530

    Add files via upload

---
+++ Ten-FireBase github/src/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "reactstrap";
+
+const Footer = () => (
+  <Container
+    fluid
+    tag="footer"
+    className="text-center bg-info text-white text-uppercase fixed-bottom p-3"
+  >
+    GitHub serch app with firebase
+  </Container>
+);
+
+export default Footer;
